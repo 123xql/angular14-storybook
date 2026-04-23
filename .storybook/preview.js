891ca5d332e6eb20b472ac627d281ad25cd5d1ad
xuqilei <xuqilei@protosoft.cn>
2026-04-23 10:25:14 +0800

    修改storybook配置

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,8 +7,9 @@ export const parameters = {
     },
   },
   docs: {
-    // 这个选项是关键：强制 Docs 页面使用内联框架来渲染故事
-    // 而不是使用一个独立的 iframe
     inlineStories: true,
+    source: {
+      state: 'open',        
+    },
   },
 };
